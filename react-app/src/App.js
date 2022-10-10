82ba78ed6747138216c2b1d734bd4a4e7077fb72
Steven Wang <stevenwang03@gmail.com>
2022-10-10 19:39:46 -0400

    Put code into React

---
+++ react-app/src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <><div class="logo">
+      <img src="https://clubs-cu.s3.amazonaws.com/Spectator+Publishing+Logo.png"></img>
+    </div><div>
+        <h1 class="title">Spectator's Sources</h1>
+      </div><div class="block">
+        <h2>Source Name:</h2>
+        <div class="white">
+          <h2>hi</h2>
+        </div>
+        <h2>Source Email:</h2>
+        <div class="white">
+          <h2>hi</h2>
+        </div>
+        <div class="green">
+          <h2>ADD</h2>
+        </div>
+      </div><div class="space-white">
+        <div class="source">
+          <h1>1</h1>
+          <h1>Presbo</h1>
+          <h1 class="email">presbo@columbia.edu</h1>
+          <div class="red">
+            <h2>DELETE</h2>
+          </div>
+        </div>
+
+        <div class="source">
+          <h1>2</h1>
+          <h1>John Jay Mouse</h1>
+          <h1 class="email">mouse@columbia.edu</h1>
+          <div class="red">
+            <h2>DELETE</h2>
+          </div>
+        </div>
+
+        <div class="source">
+          <h1>1</h1>
+          <h1>Water Bottle Man</h1>
+          <h1 class="email">flipper@columbia.edu</h1>
+          <div class="red">
+            <h2>DELETE</h2>
+          </div>
+        </div>
+      </div></>
+  );
+}
+
+export default App;

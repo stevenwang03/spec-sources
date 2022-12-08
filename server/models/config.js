019eafac70f8356e0b5628389cbb4a617001144f
Steven Wang <stevenwang03@gmail.com>
2022-12-07 20:31:57 -0500

    fixed .env file

--- server/models/config.js
+++ server/models/config.js
@@ -1,10 +1,11 @@
-require('dotenv').config();
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, '../../.env')});
 
 const dbUser = process.env.DBUSER;
 const dbName = process.env.DBNAME;
 const dbPassword = process.env.DBPASSWORD;
 const discardAfterXDays = 14;
-const URI = `mongodb+srv://{User}:{Password}@cluster0.x8xhfdw.mongodb.net/{Name}?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.x8xhfdw.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 
 module.exports = {
     URI: URI,

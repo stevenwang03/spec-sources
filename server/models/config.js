102434fb30c9c5716067f60bf181dfe7b794de93
Steven Wang <stevenwang03@gmail.com>
2022-12-07 20:18:01 -0500

    Did NOT push password

--- server/models/config.js
+++ server/models/config.js
@@ -4,7 +4,7 @@ const dbUser = process.env.DBUSER;
 const dbName = process.env.DBNAME;
 const dbPassword = process.env.DBPASSWORD;
 const discardAfterXDays = 14;
-const URI = `mongodb+srv://stevenwang03:5kQpMYNVdi3DhpYf@cluster0.x8xhfdw.mongodb.net/Spec_Sources_Collections?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://{User}:{Password}@cluster0.x8xhfdw.mongodb.net/{Name}?retryWrites=true&w=majority`;
 
 module.exports = {
     URI: URI,
